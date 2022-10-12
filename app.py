70aa76dc8ee772d6e62c2bb8f310a02919f7e768
aeferreira <ferreiae@gmail.com>
2022-10-12 21:33:03 +0100

    app.py simplified and secondary plots added.

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def hanes_woolf(a, v0):
     Km = b / m
     SV = V * Sm / m
     SKm = Km * np.sqrt((Sm/m)**2 + (Sb/b)**2)
-    return res_dict('Hanes or Woolf', V, Km, SE_V=SV, SE_Km=SKm,
+    return res_dict('Hanes', V, Km, SE_V=SV, SE_Km=SKm,
                       x=x, y=y, m=m, b=b)
 
 
@@ -163,90 +163,157 @@ def cornish_bowden(a, v0):
     res['straights_b'] = v0
     return res
 
-
 def compute_methods(a, v0):
     # compute methods
     m_table  = (hyperbolic, lineweaver_burk, hanes_woolf, eadie_hofstee, cornish_bowden)
-    all_r = [m(a, v0) for m in m_table]
-    return MethodResults(a, v0, all_r)
+    results = [m(a, v0) for m in m_table]
+    return {'a': a, 'v0': v0, 'results': results}
+    #return MethodResults(a, v0, all_r)
 
+# ------------- (str) report of results ------
 
-# ------------ all methods object, with plots --------------------------
+def repr_x_deltax(x, deltax):
+    if deltax is None:
+        return f'{x:6.3f}'
+    return f"{x:6.3f} ± {deltax:6.3f}"
+
+def report_str(results):
+    lines = []
+    for result in results:
+        lines.append(result['name'])
+        lines.append('   V  = ' + repr_x_deltax(result['V'], result['SE_V']))
+        lines.append('   Km = ' + repr_x_deltax(result['Km'], result['SE_Km']))
+    return '\n'.join(lines)
+
+# ------------ plots --------------------------
+
 default_color_scheme = ('darkviolet',
                         'green',
                         'darkred',
                         'cornflowerblue',
                         'goldenrod')
 
-class MethodResults(object):
-    
-    def __init__(self, a, v0, results_list):
-        self.results=results_list
-        self.a = a
-        self.v0 = v0
+# constants
+demo_data = """0.138 0.148
+0.220 0.171
+0.291 0.234
+0.560 0.324
+0.766 0.390
+1.460 0.493
+"""
 
-    def repr_x_deltax(self, x, deltax):
-        if deltax is None:
-            return f'{x:6.3f}'
-        return f"{x:6.3f} ± {deltax:6.3f}"
+empty_df = pd.DataFrame({'substrate': [0], 'rate':[0]})
+empty_df.index.name = '#'
 
-    def report_str(self):
-        lines = []
-        for result in self.results:
-            lines.append(result['name'])
-            lines.append('   Vmax = ' + self.repr_x_deltax(result['V'], result['SE_V']))
-            lines.append('   Km   = ' + self.repr_x_deltax(result['Km'], result['SE_Km']))
-        return '\n'.join(lines)
+fig0 = Figure(figsize=(8, 6))
+ax0 = fig0.subplots()
+t = ax0.text(0.5, 0.5, 'no figure generated')
 
-    def __str__(self):
-        results = self.results
-        col_labels = ["Method                V      SE_V   Km     SE_Km"]
-        fstring = '{:20} {:6.3f} {:6.3f} {:6.3f} {:6.3f}'
-        mlines = []
-        for r in results:
-            if r['SE_V'] is None:
-                SE_V = float('nan')
-            else:
-                SE_V = r['SE_V']
-            if r['SE_Km'] is None:
-                SE_Km = float('nan')
-            else:
-                SE_Km = r['SE_Km']
-            mlines.append(fstring.format(r['name'], r['V'], SE_V, r['Km'], SE_Km))
-        col_labels.extend(mlines)
-        return '\n'.join(col_labels)
+last_results = None
+all_methods_list = ['Hyperbolic Regression',
+                    'Lineweaver-Burk',
+                    'Hanes',
+                    'Eadie-Hofstee',
+                    'Eisenthal-C.Bowden']
 
-    def plot_others(self, colorscheme=None, grid=True):
-        a = self.a
-        v0 = self.v0
-        all_r = self.results
-        if colorscheme is None:
-            colorscheme = default_color_scheme
-        f, ax = plt.subplots(2, 2, figsize=(10,7.5))
-        ax = [ax[0][0], ax[0][1], ax[1][0], ax[1][1]]
-        for i in range(0,3):
-             draw_lin_plot(ax[i], all_r[i], color=colorscheme[i])
-        draw_cornish_bowden_plot(ax[3], all_r[4], color=colorscheme[4])
-        f.tight_layout()
+def read_data(data):
+    a = []
+    v0 = []
+    for line in data.splitlines():
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        x1, x2 = line.split(None, 2)
+        try:
+            x1, x2 = float(x1), float(x2)
+        except:
+            continue
+        a.append(x1)
+        v0.append(x2)
+    return np.array(a), np.array(v0)
 
-        plt.show()
-        return f
+def hypers_mpl(results, display_methods=all_methods_list,
+                      colorscheme=None, 
+                      title=None,
+                      legend=True,
+                      grid=True):
+
+    default_color_scheme = ('darkviolet',
+                            'green',
+                            'darkred',
+                            'cornflowerblue',
+                            'goldenrod')
+    a = results['a']
+    v0 = results['v0']
+    all_results = results['results']
+    plt.rc('mathtext', fontset='cm')
+    f, ax = plt.subplots(1,1, figsize=(8,6))
+
+    if colorscheme is None:
+        colorscheme = default_color_scheme
+
+    xmax = max(a) * 1.1
+    ymax = max(v0) * 1.1
+
+    if title is not None:
+        ax.set_title(title)
+    ax.set_ylim(0, ymax)
+    ax.set_xlim(0, xmax)
+    chosen3letter = [choice[:3] for choice in display_methods]
+
+    for result, color in zip(all_results, colorscheme):
+        if result['name'][:3] not in chosen3letter:
+            continue
+
+        x, y = MM_line(result['V'], result['Km'], xmax=xmax)
+
+        ax.plot(x, y, label=result['name'],
+                      color=color,
+                      linestyle='solid',
+                      lw=2)
+
+    ax.plot(a, v0, marker='o',
+                   linestyle='None', 
+                   markerfacecolor='white', 
+                   markeredgecolor='black', 
+                   markeredgewidth=1.5, 
+                   markersize=6)
+    ax.set_xlabel('$a$', fontsize=16)
+    ax.set_ylabel('$v_o$', fontsize=16)
+    if legend:
+        ax.legend(loc='lower right')
+    if grid:
+        ax.grid()
+    return f
+
+def plot_others_mpl(results, colorscheme=None, grid=True):
+    all_r = results['results']
+    if colorscheme is None:
+        colorscheme = default_color_scheme
+    plt.rc('mathtext', fontset='cm')
+    f, ax = plt.subplots(2, 2, figsize=(10,7.5))
+    ax = [ax[0][0], ax[0][1], ax[1][0], ax[1][1]]
+    for i in range(0,3):
+            draw_lin_plot(ax[i], all_r[i+1], color=colorscheme[i+1], grid=grid)
+    draw_cornish_bowden_plot(ax[3], all_r[4], color=colorscheme[4], grid=grid)
+    f.tight_layout()
+    return f
 
 
-def draw_lin_plot(ax, results, color='black', 
+def draw_lin_plot(ax, result, color='black', 
                                title=None,
                                grid=True):
 
     if title is None:
-        title = results['name']
-    x = results['x']
-    y = results['y']
+        title = result['name']
+    x = result['x']
+    y = result['y']
 
     xmax = max(x) * 1.1
-    ymax = xmax * results['m'] + results['b']
+    ymax = xmax * result['m'] + result['b']
 
-    if results['m'] < 0:
-        ytop = results['b']
+    if result['m'] < 0:
+        ytop = result['b']
     else:
         ytop = ymax
     ytop = 1.1 * ytop
@@ -254,8 +321,8 @@ def draw_lin_plot(ax, results, color='black',
     ax.set_title(title)
     ax.set_ylim(0, ytop)
     ax.set_xlim(0, xmax)
-    
-    ax.plot([0,xmax], [results['b'], ymax], color=color,
+
+    ax.plot([0,xmax], [result['b'], ymax], color=color,
                   linestyle='solid',
                   lw=2)
 
@@ -266,15 +333,15 @@ def draw_lin_plot(ax, results, color='black',
                    markeredgewidth=1.5, 
                    markersize=6)
 
-    if results.name.startswith('Lineweaver'):
-        xlabel = '1/[S]'
-        ylabel = '$1/v_0$'
-    elif results.name.startswith('Hanes'):
-        xlabel = '[S]'
-        ylabel = '[S]/$v_0$'
-    elif results.name.startswith('Eadie'):
-        xlabel = '$v_0$/[S]'
-        ylabel = '$v_0$'
+    if result['name'].startswith('Lineweaver'):
+        xlabel = '$1/a$'
+        ylabel = '$1/v_o$'
+    elif result['name'].startswith('Hanes'):
+        xlabel = '$a$'
+        ylabel = '$a/v_o$'
+    elif result['name'].startswith('Eadie'):
+        xlabel = '$v_o/a$'
+        ylabel = '$v_o$'
     else:
         xlabel = ''
         ylabel = ''
@@ -282,7 +349,6 @@ def draw_lin_plot(ax, results, color='black',
     ax.set_ylabel(ylabel)
     if grid:
         ax.grid()
-    
 
 def draw_cornish_bowden_plot(ax, results, 
                                  color='black',
@@ -331,97 +397,6 @@ def draw_cornish_bowden_plot(ax, results,
     if grid:
         ax.grid()
 
-# constants
-demo_data = """0.138 0.148
-0.220 0.171
-0.291 0.234
-0.560 0.324
-0.766 0.390
-1.460 0.493
-"""
-empty_df = pd.DataFrame({'substrate': [0], 'rate':[0]})
-empty_df.index.name = '#'
-
-fig0 = Figure(figsize=(8, 6))
-ax0 = fig0.subplots()
-t = ax0.text(0.5, 0.5, 'no figure generated')
-
-last_results = None
-all_methods_list = ['Hyperbolic Regression', 'Lineweaver-Burk', 'Hanes-Woolf', 'Eadie-Hofstee', 'Eisenthal-C.Bowden']
-
-def read_data(data):
-    a = []
-    v0 = []
-    for line in data.splitlines():
-        line = line.strip()
-        if len(line) == 0:
-            continue
-        x1, x2 = line.split(None, 2)
-        try:
-            x1, x2 = float(x1), float(x2)
-        except:
-            continue
-        a.append(x1)
-        v0.append(x2)
-    return np.array(a), np.array(v0)
-
-
-def hypers_mpl(results, display_methods=all_methods_list,
-                      colorscheme=None, 
-                      title=None,
-                      legend=True,
-                      grid=True):
-
-    default_color_scheme = ('darkviolet',
-                            'green',
-                            'darkred',
-                            'cornflowerblue',
-                            'goldenrod')
-    a = results.a
-    v0 = results.v0
-    all_results = results.results
-
-    f = Figure(figsize=(8, 6))
-    ax = f.add_subplot()
-    
-    if colorscheme is None:
-        colorscheme = default_color_scheme
-
-    xmax = max(a) * 1.1
-    ymax = max(v0) * 1.1
-
-    if title is not None:
-        ax.set_title(title)
-    ax.set_ylim(0, ymax)
-    ax.set_xlim(0, xmax)
-    chosen3letter = [choice[:3] for choice in display_methods]
-
-    for result, color in zip(all_results, colorscheme):
-        if result['name'][:3] not in chosen3letter:
-            continue
-
-        x, y = MM_line(result['V'], result['Km'], xmax=xmax)
-
-        ax.plot(x, y, label=result['name'],
-                      color=color,
-                      linestyle='solid',
-                      lw=2)
-
-    ax.plot(a, v0, marker='o',
-                   linestyle='None', 
-                   markerfacecolor='white', 
-                   markeredgecolor='black', 
-                   markeredgewidth=1.5, 
-                   markersize=6)
-    ax.set_xlabel('$a$')
-    ax.set_ylabel('$v_0$', fontsize=16)
-    if legend:
-        ax.legend(loc='lower right')
-    if grid:
-        ax.grid()
-    return f
-
-
 def read_data_df(data_text):
     tf = StringIO(data_text)
     df = pd.read_csv(tf, delim_whitespace=True, comment='#', index_col=False)
@@ -455,7 +430,7 @@ def b_clear(event):
     app_column[-1] = no_results_pane
 clear_button.on_click(b_clear)
 
-edit_table_group = pn.widgets.RadioButtonGroup(options=['Edit', 'Table'], width=100)
+edit_table_group = pn.widgets.RadioButtonGroup(options=['Edit', 'Check'], width=100)
 edit_table_group.value = 'Edit'
 
 demo_button = pn.widgets.Button(name='Demo data', width=200)
@@ -464,15 +439,12 @@ def b_demo(event):
     edit_table_group.value = 'Edit'
 demo_button.on_click(b_demo)
 
-def transform_data2df(text):
-    s, v0 = read_data(text)
-    df = pd.DataFrame({'rate':v0}, index=s)
-    df.index.name = 'substrate'
-    return df
-    
 def change_data_view(event):
-    if event.new == 'Table':
-        df = transform_data2df(data_input_text.value)
+    if event.new == 'Check':
+        a, v0 = read_data(data_input_text.value)
+        df = pd.DataFrame({'rate':v0}, index=a)
+        df.index.name = 'substrate'
+
         data_dfwidget.value = df
         data_input_column[0] = data_dfwidget
         pn.state.notifications.position = 'top-left'
@@ -488,6 +460,8 @@ results_text = pn.pane.Str('', style={'font-family': "monospace"})
 
 mpl_pane = pn.pane.Matplotlib(fig0)
 
+mpl_others_pane = pn.pane.Matplotlib(fig0)
+
 check_methods = pn.widgets.CheckBoxGroup(options=all_methods_list, value=all_methods_list, inline=False)
 
 @pn.depends(check_methods)
@@ -497,6 +471,13 @@ def change_plot(check_methods):
         f = hypers_mpl(last_results, display_methods=check_methods)
         mpl_pane.object = f
     return mpl_pane
+
+def draw_other_plots():
+    global last_results
+    if last_results is not None:
+        f = plot_others_mpl(last_results)
+        mpl_others_pane.object = f
+    return mpl_others_pane
 
 @pn.depends(check_methods)
 def get_png_hypers(check_methods):
@@ -525,7 +506,6 @@ fd_pdf = pn.widgets.FileDownload(callback=get_pdf_hypers, filename='hypers.pdf',
 
 no_results_pane = pn.Column(None)
 
-
 # the "Fit" button
 fit_button = pn.widgets.Button(name='Fit', width=200, button_type='primary')
 def b_fit(event):
@@ -533,9 +513,13 @@ def b_fit(event):
     s, v0 = read_data(data_input_text.value)
     last_results = compute_methods(s, v0)
 
-    results_text.object = last_results.report_str()
+    results_text.object = report_str(last_results['results'])
+
+    otherp = draw_other_plots()
+
     results_pane = pn.Column(pn.layout.Divider(), "### Fitting results", results_text, '### Plots',
-                         pn.Row(change_plot, pn.Column(pn.Spacer(height=50), check_methods, fd_png, fd_pdf)))
+                         pn.Row(change_plot, pn.Column(pn.Spacer(height=50), check_methods, fd_png, fd_pdf)),
+                         pn.Row(otherp))
     app_column[-1] = results_pane
 fit_button.on_click(b_fit)
 
@@ -544,7 +528,9 @@ data_input_row = pn.Row(data_input_column, pn.Column(top_buttons, demo_button, f
 
 header = """## Michaelis-Menten equation fitting
 
-$$v = \\frac{V a}{K_m + a}$$
+$$v_o = \\frac{V a}{K_m + a}$$
+
+by António Ferreira
 
 ### Data input
 """
